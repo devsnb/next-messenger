8381248e6765bd9a270bc090054d45c3636be8af
Snehangshu Biswas <dev.snehangshu@gmail.com>
2024-01-02 16:53:25 +0530

    prepared for deployment

--- actions/getSession.ts
+++ actions/getSession.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth'
 
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/utils/authOptions'
 
 export default async function getSession() {
 	return await getServerSession(authOptions)
